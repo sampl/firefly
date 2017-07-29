b93820030eec8edc5bca4137d9a2e7867fb2b33b
Sam Pierce Lolla <sampl@Sams-MacBook-Pro.local>
2017-07-29 08:28:59 -0400

    use json file for sample script import
    - also rename files for clarity

--- scripts/sample_script.js
+++ scripts/sample_script.js
@@ -6,6 +6,8 @@ var serviceAccountProd = require("../config/service-account-key-prod.json")
 var firebaseConfigDev = require("../config/firebase-config-dev.json")
 var firebaseConfigProd = require("../config/firebase-config-prod.json")
 
+var posts = require("./data/sample_posts.json")
+
 // use on production server with `node test.js prod`, otherwise falls back to dev
 // https://firebase.google.com/docs/database/admin/start
 if (process.argv[2] == 'prod') {
@@ -20,19 +22,15 @@ if (process.argv[2] == 'prod') {
   })
 }
 
-var db = admin.database()
-var ref = db.ref('/post')
+var database_ref = admin.database().ref('/post')
 
-var testPost = {
-  title: 'Created from Script',
-  content: 'This post was created by running a test script locally'
-}
-
-var key = ref.push().key
-ref.child(key).update(testPost, function(err) {
-  if (err) {
-    console.error('something went wrong: ', err)
-  } else {
-    console.log('created post '+key)
-  }
+posts.forEach(function(post) {
+  var key = database_ref.push().key
+  database_ref.child(key).update(post, function(err) {
+    if (err) {
+      console.error('something went wrong: ', err)
+    } else {
+      console.log('created post '+key)
+    }
+  })
 })
