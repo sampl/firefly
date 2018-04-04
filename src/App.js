e241c0ad96ace68f5ce9eaa0a86c9004c0554587
Sam Pierce Lolla <sampiercelolla@gmail.com>
2018-04-04 06:29:54 -0400

    clean up CRA

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <h1>Firefly</h1>
+    <p>Hello world</p>
+  </div>
+)
 
-export default App;
+export default App
