137a6d39b2b085a7e149e5ef1a9e6e1f33e707d9
Sam Pierce Lolla <sampiercelolla@gmail.com>
2018-04-04 06:51:25 -0400

    add firebase with environment config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Firebase from 'firebase'
+// eslint-disable-next-line
+import Firestore from 'firebase/firestore'
+
 import App from './App'
+
+// DATABASE
+const dbConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+}
+let FirebaseApp = Firebase.initializeApp(dbConfig)
+window.firebase = FirebaseApp // debugging
 
 ReactDOM.render(<App />, document.getElementById('root'))
